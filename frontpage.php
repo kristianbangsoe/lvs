2576d762aed5edd5bc06ceda1699f0cef93cb024
Kristian Bangsø <kette_bangsoe@live.dk>
2018-04-03 13:31:23 +0200

    How to make the skeleton
    Here you go sir

--- frontpage.php
+++ frontpage.php
@@ -2,8 +2,46 @@
 
 <!-- The page content here -->
 
-<p>Add your html here</p>
 
-<!-- The page content ends here -->
+<section id="section-1" style="background-image: url('assets/images/header.jpg')">
+  <div class="container">
+    <h1>Headline</h1>
+    <p>Context</p>
+    <form class="" action="index.html" method="post">
+      <div class="form-item">
+        <label for="area">Vælg region</label>
+        <select class="" name="area">
+          <option value="[object Object]">Fyn</option>
+        </select>
+      </div>
+      <div class="form-item">
+        <label for="area">Vælg region</label>
+        <select class="" name="area">
+          <option value="[object Object]">Fyn</option>
+        </select>
+      </div>
+      <div class="form-item">
+        <label for="area">Vælg region</label>
+        <select class="" name="area">
+          <option value="[object Object]">Fyn</option>
+        </select>
+      </div>
+    </form>
+  </div>
+</section>
+
+<section id="categories">
+
+</section>
+
+<section id="events">
+
+</section>
+
+<section id="reviews">
+
+</section>
+
+<!-- And so on -->
 
 <?php include 'footer.php'; ?>
