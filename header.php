63e27cf3b2aff6cbbfeaf52acb1f224c9ed8daad
Joergen <Jurgenandre101@hotmail.com>
2018-04-04 14:47:13 +0200

    Header ikke responsiv
    Header ikke responsiv

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Om LVS <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="about.php">Om LVS <span class="sr-only">(current)</span></a>
             </li>
 
 
