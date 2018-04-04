1638273363e3ef29dca5998296bf8a19d937ec61
Joergen <Jurgenandre101@hotmail.com>
2018-04-04 22:36:45 +0200

    Forside
    Forside

--- frontpage.php
+++ frontpage.php
@@ -3,10 +3,10 @@
 <!-- The page content here -->
 
 
-<section id="section-1" style="background-image: url('assets/images/header.jpg')">
-  <div class="container">
-    <h1>Headline</h1>
-    <p>Context</p>
+<section id="section-1">
+  <div class="container-fluid">
+    <h1 class="boldtext">Over <span class="colorgreentext"> 130 væresteder</span> dedikeret til</h1>
+    <p>Landsforeningen af VæreSteder samler alle væresteder i danmark, til socialt udsatte der mangler hjælp. Du vil kunne finde kontakt information, event kalendre og meget mere for de forskellige væresteder. </p>
     <form class="" action="index.html" method="post">
       <div class="form-item">
         <label for="area">Vælg region</label>
@@ -26,12 +26,16 @@
           <option value="[object Object]">Fyn</option>
         </select>
       </div>
+      <button type="button" name="button">SØG</button>
     </form>
   </div>
 </section>
 
-<section id="categories">
-
+<section id="categories" class= "col-md-12">
+<div class="container">
+  <h1 class="boldtext">Hvad Landsforeningen <span class="colorgreentext">tilbyder</span></h1>
+<p> </p>
+</div>
 </section>
 
 <section id="events">
