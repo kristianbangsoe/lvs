9ba029bb4a3208e17002464a6fc98bf82e63eb4b
Kristian Bangsø <kette_bangsoe@live.dk>
2018-04-05 16:44:50 +0200

    Auto stash before merge of "master" and "origin/master"
    Fixed header
    Footer
    And section one
    Added animation script

--- frontpage.php
+++ frontpage.php
@@ -3,31 +3,55 @@
 <!-- The page content here -->
 
 
-<section id="section-1">
-  <div class="container-fluid">
-    <h1 class="boldtext">Over <span class="colorgreentext"> 130 væresteder</span> dedikeret til</h1>
-    <p>Landsforeningen af VæreSteder samler alle væresteder i danmark, til socialt udsatte der mangler hjælp. Du vil kunne finde kontakt information, event kalendre og meget mere for de forskellige væresteder. </p>
-    <form class="" action="index.html" method="post">
-      <div class="form-item">
-        <label for="area">Vælg region</label>
-        <select class="" name="area">
-          <option value="[object Object]">Fyn</option>
-        </select>
+<section id="section-1" style="background-image: url(assets/images/header.jpg);">
+  <div class="container">
+    <div class="row">
+      <div class="col-7 wow fadeInLeft">
+        <h1 class="pb-3">Over <span class="boldtext colorgreentext"> 130 væresteder</span> dedikeret til de socialt udsatte i Danmark</h1>
+        <p>Landsforeningen af VæreSteder samler alle væresteder i danmark, til socialt udsatte der mangler hjælp. Du vil kunne finde kontakt information, event kalendre og meget mere for de forskellige væresteder. </p>
+        <form class="" action="index.html" method="post">
+          <div class="form-group">
+            <div class="form-item">
+              <label for="center-area">Vælg region</label>
+              <select class="" name="area">
+                <option value="[object Object]">Fyn</option>
+                <option value="[object Object]">Sjælland</option>
+                <option value="[object Object]">Nord Jylland</option>
+                <option value="[object Object]">Midt Jylland</option>
+                <option value="[object Object]">Sønder Jylland</option>
+                <option value="[object Object]">Bornholm</option>
+              </select>
+            </div>
+            <div class="form-item">
+              <label for="center-type">Vælg type</label>
+              <select class="" name="center-type">
+                <option value="[object Object]">Alle</option>
+                <option value="[object Object]">Underholdning</option>
+                <option value="[object Object]">Café</option>
+                <option value="[object Object]">Hjemløse</option>
+                <option value="[object Object]">Misbrug</option>
+              </select>
+            </div>
+            <div class="form-item">
+              <label for="center-name">Vælg Værested</label>
+              <select class="" name="center-name">
+                <option selected disabled hidden value="[object Object]">Se tilgængelige</option>
+                <option value="[object Object]">Dagvarmestuen</option>
+                <option value="[object Object]">Natvarmestuen</option>
+                <option value="[object Object]">Paraplyen</option>
+                <option value="[object Object]">Reden i Odense</option>
+                <option value="[object Object]">Stoppestedet - SIND værested</option>
+                <option value="[object Object]">Vista Balboa</option>
+                <option value="[object Object]">Værestedet Lyset</option>
+                <option value="[object Object]">Café Mælkevejen</option>
+                <option value="[object Object]">Blochhuset</option>
+              </select>
+            </div>
+          </div>
+          <button class="rounded-btn btn-white" type="button" name="button">SØG</button>
+        </form>
       </div>
-      <div class="form-item">
-        <label for="area">Vælg region</label>
-        <select class="" name="area">
-          <option value="[object Object]">Fyn</option>
-        </select>
-      </div>
-      <div class="form-item">
-        <label for="area">Vælg region</label>
-        <select class="" name="area">
-          <option value="[object Object]">Fyn</option>
-        </select>
-      </div>
-      <button type="button" name="button">SØG</button>
-    </form>
+    </div>
   </div>
 </section>
 
--- header.php
+++ header.php
@@ -5,76 +5,73 @@
     <meta charset="utf-8">
     <title>LVS</title>
     <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
     <header>
 
+<nav class="navbar navbar-expand-lg navbar-light headerbackgroundcolor">
+  <div class="container">
 
-      <nav class="navbar navbar-expand-lg navbar-light headerbackgroundcolor">
-<div class="containertest">
+    <a href="frontpage.php">
+    <img class="logo" src="assets/images/logo.svg" alt="">
+    </a>
 
-<a href="frontpage.php">
-<img class="logo" src="assets/images/logo.svg" alt="" height="240" width="290">
-</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="about.php">Om LVS <span class="sr-only">(current)</span></a>
+        </li>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="about.php">Om LVS <span class="sr-only">(current)</span></a>
-            </li>
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Projekter
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Projekter1</a>
+            <a class="dropdown-item" href="#">Projekter2</a>
+            <a class="dropdown-item" href="#">Projekter3</a>
+          </div>
+        </li>
 
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Aktiviteter
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Aktiviteter1</a>
+            <a class="dropdown-item" href="#">Aktiviteter2</a>
+            <a class="dropdown-item" href="#">Aktiviteter3</a>
+          </div>
+        </li>
 
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Udgivelser
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Udgivelser1</a>
+            <a class="dropdown-item" href="#">Udgivelser2</a>
+            <a class="dropdown-item" href="#">Udgivelser3</a>
+          </div>
+        </li>
 
-
-            <li class="nav-item dropdown active">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Projekter
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Projekter1</a>
-                <a class="dropdown-item" href="#">Projekter2</a>
-                <a class="dropdown-item" href="#">Projekter3</a>
-              </div>
-            </li>
-
-            <li class="nav-item dropdown active">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Aktiviteter
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Aktiviteter1</a>
-                <a class="dropdown-item" href="#">Aktiviteter2</a>
-                <a class="dropdown-item" href="#">Aktiviteter3</a>
-              </div>
-            </li>
-
-            <li class="nav-item dropdown active">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Udgivelser
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Udgivelser1</a>
-                <a class="dropdown-item" href="#">Udgivelser2</a>
-                <a class="dropdown-item" href="#">Udgivelser3</a>
-              </div>
-            </li>
-
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Kontakt</a>
-            </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Kontakt</a>
+        </li>
 
 
 
-          </ul>
+      </ul>
 
-      <button class="buttonheader" type="button" name="button">Find et V&aelig;rested</button>
+      <button class="btn-green" type="button" name="button">Find et Værested</button>
 
-        </div>
-        </div>
-      </nav>
+    </div>
+  </div>
+</nav>
     </header>
