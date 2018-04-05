edadfeed4446a60f7af36732ac87d3d862b6e274
Kristian Bangsø <kette_bangsoe@live.dk>
2018-04-05 08:51:24 +0200

    Auto stash before merge of "master" and "origin/master"
    And added footer

--- footer.php
+++ footer.php
@@ -1,5 +1,45 @@
 
-<footer></footer>
+<footer>
+  <div class="container py-5 fd-row d-flex jc-between" >
+    <div class="col-3">
+      <img id="logo-white" class="pb-4" src="assets/images/logo-white.svg" alt="">
+      <p>Danmarksgade 6A, 1.<br>7000 Fredericia</p>
+      <p>
+        Tlf. 7592 4000<br>
+        Lvsinfo@lavs.dk<br>
+        EAN-nr. 579 800 647 6909<br>
+        Cvr.nr. 29055009
+      </p>
+    </div>
+
+    <div class="col-3">
+      <h2 class="h5">Om landsforeningen</h2>
+      <ul>
+        <li><a href="#">Om LVS</a></li>
+        <li><a href="#">Væresteder</a></li>
+        <li><a href="#">Presse</a></li>
+        <li><a href="#">Job hos os</a></li>
+        <li><a href="#">Kontakt os</a></li>
+      </ul>
+    </div>
+
+    <div class="col-3">
+      <h2 class="h5">Viden</h2>
+      <ul>
+        <li><a href="#">Om LVS</a></li>
+        <li><a href="#">Væresteder</a></li>
+        <li><a href="#">Presse</a></li>
+        <li><a href="#">Job hos os</a></li>
+        <li><a href="#">Kontakt os</a></li>
+      </ul>
+    </div>
+
+    <div class="">
+      <img id="mobil-pay" src="assets/images/mobil-pay.svg" alt="">
+    </div>
+
+  </div>
+</footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
