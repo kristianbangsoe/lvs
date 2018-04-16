f94f8b8055cc9be528f5a08b07e9c68b7a47028d
Unknown <prat0053@edu.eal.dk>
2018-04-16 11:38:31 +0200

    Vaeresteder
    Skeleton Basic v,01

--- vaeresteder.php
+++ vaeresteder.php
@@ -2,9 +2,137 @@
 <?php include 'header.php'; ?>
 
 <!-- The page content here -->
+<div id="profile">
+  <section class="cover" style="background-image: url(assets/images/preben.png);">
+    <div class="overlay-dark">
+      <div class="container">
+        <div class="user row">
+          <div class="col-9 d-flex ai-center">
+            <div class="back mr-3">
+              <a href="#"><i class="fas fa-arrow-left"></i></a>
+              <h6 class="ml-5 mb-0">Gå tilbage</h6>
+            </div>
+          </div>
+          <h1 class="ml-3 mb-5 ">Ifølge Preben Brandt</h1>
+        </div>
+          </div>
+        </div>
+  </section>
 
-<p>Add your html here</p>
+</div>
+<div class="container">
+  <div class="row">
+    <aside class="col-3">
+      <div class="row pt-5 bl br bb bb-rounded">
+        <div class="">
+          <div class="col">
+            <h2 class="sub-title mt-5 boldtext mb-3">Relaterede artikler</h2>
+            <address class="[object Object]">
+              <p><i class=""></i>Er civiliseret sammenfund</p>
+              <p><i class=""></i>Vendepunktet</p>
+              <p><i class=""></i>Social ulighed i sundhed</p>
+              <p><i class=""></i>Nyt om narkotikarelatered…</p>
+              </address>
+          </div>
+            <h2 class="sub-title mt-5 boldtext mb-3">Seneste nyt</h2>
+          <address class="[object Object]">
+            <p><i class=""></i>Er civiliseret sammenfund</p>
+            <p><i class=""></i>Vendepunktet</p>
+            <p><i class=""></i>Social ulighed i sundhed</p>
+            <p><i class=""></i>Nyt om narkotikarelatered…</p>
+            </address>
+        </div>
+        <div class="bt col py-3 host d-flex">
 
+
+        </div>
+        <div class="w-100 d-flex">
+      </div>
+      </div>
+
+
+
+        <div class="col events mt-5">
+  <div class="d-flex row">
+    <figure class="event-item col-20">
+      <div class="social-bar jc-end d-flex col-3">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fas fa-globe"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+      </div>
+    </figure>
+
+
+    </div>
+
+        </div>
+
+      <div class="col events mt-5">
+
+        <div class="d-flex row">
+
+          <figure class="event-item col-20">
+            <a href="#">
+              <div class="event-image" style="background-image: url('assets/images/event-1.jpg');">
+                <div class="overlay p-3">
+                  <div class="date-dm text-right"><div class="date-day">20</div>MAR</div>
+                  <h2 class="event-title">CAMPING TUR PÅ RØMØ </h2>
+                </div>
+              </div>
+              <div class="event-info px-3 py-4">
+                <div class="btn-green-round">
+                  <i class="read-more-btn fas fa-arrow-right"></i>
+                </div>
+                <p class="date">På Værestedet Regnbuen i Kolding
+får de i dag besøg af en dyrlæge,
+som gratis vaccinerer brugernes
+dyr. Det er den…</p>
+
+</div>
+            </a>
+          </figure>
+
+
+        </div>
+      </div>
+    </aside>
+    <div class="description-container col-8 py-5 pl-5">
+      <div class="col">
+        <h3 class="h3 boldtext">Frivillighedsåret og sundhed</h3>
+        <div class="description">
+          <p class="boldtext">Over seks måneder har Preben Brandt (dr.med.) leveret sit syn på forskellige emner, som alle kredser om socialt betinget ulighed i sundhed. I denne sidste artikel i rækken er frivillighed i sundhedssystemet i fokus.</p>
+  <h3 class="h3 boldtext">Mangfoldighed er vores styrke</h3>
+  Som fængselsbetjent skal du både kunne sætte dig ind i andres følelser, håndtere konflikter og arbejde i de rammer,
+som findes inden for vores verden. Fængselsbetjenten skal altid balancere mellem på den ene side kontrol, sikkerhed
+og tryghed, og på den anden side resocialisering, altså at støtte og motivere indsatte til at leve en kriminalitetsfri
+tilværelse.
+I
+        </div>
+        <div class="gradiant-read active">
+
+        </div>
+        <div class="text-center">
+          <div class="read-more">LÆS MERE</div>
+        </div>
+      </div>
+
+      <div class="col gallery mt-5">
+        <h3 class="sub-title bb mb-3 pb-2">Galleri</h3>
+        <div class="d-flex">
+          <a><img src="assets/images/food-1.jpg" alt=""></a>
+          <a><img src="assets/images/food-2.jpg" alt=""></a>
+          <a><img src="assets/images/food-3.jpg" alt=""></a>
+          <a><img src="assets/images/food-4.jpg" alt=""></a>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</div>
+</div>
+<div class="overlay-dark-o">
+</div>
 <!-- The page content ends here -->
 
 <?php include 'footer.php'; ?>
