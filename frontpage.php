9a61c09ee4866c9442813493b94b1f3dfb57bae2
Joergen <Jurgenandre101@hotmail.com>
2018-04-17 11:26:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- frontpage.php
+++ frontpage.php
@@ -56,155 +56,341 @@
   </div>
 </section>
 
+<!-- SEKTION 1 OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
 <section id="categories" class= "col-md-12">
 <div class="container py-5">
   <div class="row">
     <div class="col-6 m-auto text-center">
       <h1 class="boldtext h2 mt-4">Hvad Landsforeningen <span class="colorgreentext">tilbyder</span></h1>
-      <p>I Danmark er socialt udsatte stadivæk et velment fenomen som der skal gøres noget ved. Her kan du se en oversigt ov er hvad</p>
+      <p>Der findes stadig I Danmark er socialt udsatte stadivæk et udbredt fænomen som vi alle kan hjælpe med at formindske. Her kan du se en oversigt over hvordan.</p>
     </div>
   </div>
   <div class="row text-center mt-5">
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-1.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>HVAD ER ET VÆRESTED?</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-2.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>RÅDGIVNING TIL VÆRESTEDER</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-3.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>UDGIVEDE PUBLIKATIONER</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-4.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>VORES ANSVARLIGHEDER</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-5.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>BLIV FRIVILLIG & HJÆLP</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
     <div class="col-md-4">
       <figure class="p-4">
         <img class="mb-4" src="assets/images/icon-6.svg" alt="">
-        <h2>hello</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>BLIV SPONSOR & PARTNER</h2>
+        <p>Et værested er et til hjælp for socialt udsatte, LVS’s arbejde består i at skabe et netværk af væresteder og opbygge et sammenthold.</p>
         <a href="#">LÆS MERE</a>
       </figure>
     </div>
   </div>
 </div>
 </section>
-
-<section id="events">
-
-  <section id="categories" class= "col-md-12">
-  <div class="container py-5">
-    <div class="row">
-      <div class="col-6 m-auto text-center">
-        <h1 class="boldtext h2 mt-4">Hvad Landsforeningen <span class="colorgreentext">tilbyder</span></h1>
-        <p>I Danmark er socialt udsatte stadivæk et velment fenomen som der skal gøres noget ved. Her kan du se en oversigt ov er hvad</p>
-      </div>
-    </div>
-    <div class="row text-center mt-5">
-      <div class="col-md-4">
-        <figure class="p-4">
-          <img class="mb-4" src="assets/images/icon-1.svg" alt="">
-          <h2>hello</h2>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#">LÆS MERE</a>
-        </figure>
-      </div>
-      <div class="col-md-4">
-        <figure class="p-4">
-          <img class="mb-4" src="assets/images/icon-2.svg" alt="">
-          <h2>hello</h2>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#">LÆS MERE</a>
-        </figure>
-      </div>
-      <div class="col-md-4">
-        <figure class="p-4">
-          <img class="mb-4" src="assets/images/icon-3.svg" alt="">
-          <h2>hello</h2>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#">LÆS MERE</a>
-        </figure>
-      </div>
-
-</section>
+<!-- SEKTION 1 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
 
 <hr>
 
-<section id="reviews" class= "col-md-12 py-5">
+<!-- SEKTION 2 OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+  <section id="events" class="col-md-12">
+  <div class="container pb-5">
     <div class="row">
-      <div class="col-6 m-auto text-center">
-        <img src="assets/images/heart.svg" alt="" height="40" width="40">
-        <h1 class="boldtext h2 mt-4">Udtalelse fra<span class="colorgreentext"> vores talspersoner</span></h1>
-        <p>I Danmark er socialt udsatte stadivæk et velment fenomen som der skal gøres noget ved. Her kan du se en oversigt ov er hvad</p>
-      </div>
-      </div>
-          <div class="row">
-  <div id="carouselExampleIndicators" class="carousel slide m-auto" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active ">
-        <img class="d-block w-100" src="assets/images/slider1.png" alt="">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/images/slider2.png" alt="">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/images/slider1.png" alt="">
+      <div class="col-6 m-auto text-center py-5">
+        <h1 class="boldtext h2 mt-4">Kommende<span class="colorgreentext"> events</span> og<span class="colorgreentext"> aktiviteter</span></h1>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+        <div class="row">
+
+          <figure class="event-item col-4">
+            <a href="#">
+              <div class="event-image" style="background-image: url('assets/images/event-1.jpg');">
+                <div class="overlay p-3">
+                  <div class="date-dm text-right"><div class="date-day">20</div>MAR</div>
+                  <h2 class="event-title">CAMPING TUR PÅ RØMØ </h2>
+                </div>
+              </div>
+              <div class="event-info px-3 py-4">
+                <div class="btn-green-round">
+                  <i class="read-more-btn fas fa-arrow-right"></i>
+                </div>
+                <p class="date">KL. 18.00 - 27 Marts, kl.10.00</p>
+                <p class="colorgreentext boldtext">12 deltagere, 5 interesseret</p>
+                <p class="location">Placering: Paraplyen, Odense C</p>
+              </div>
+            </a>
+          </figure>
+
+          <figure class="event-item col-4">
+            <a href="#">
+              <div class="event-image" style="background-image: url('assets/images/event-2.jpg');">
+                <div class="overlay p-3">
+                  <div class="date-dm text-right"><div class="date-day">20</div>MAR</div>
+                  <h2 class="event-title">GRILL AFTEN</h2>
+                </div>
+              </div>
+              <div class="event-info px-3 py-4">
+                <div class="btn-green-round">
+                  <i class="read-more-btn fas fa-arrow-right"></i>
+                </div>
+                <p class="date">KL. 18.00 - 27 Marts, kl.22.00</p>
+                <p class="colorgreentext boldtext">32 deltagere, 7 interesseret</p>
+                <p class="location">Placering: Tusind års skoven, Odense C</p>
+              </div>
+            </a>
+          </figure>
+
+          <figure class="event-item col-4">
+            <a href="#">
+              <div class="event-image" style="background-image: url('assets/images/event-2.jpg');">
+                <div class="overlay p-3">
+                  <div class="date-dm text-right"><div class="date-day">20</div>MAR</div>
+                  <h2 class="event-title">GRILL AFTEN</h2>
+                </div>
+              </div>
+              <div class="event-info px-3 py-4">
+                <div class="btn-green-round">
+                  <i class="read-more-btn fas fa-arrow-right"></i>
+                </div>
+                <p class="date">KL. 18.00 - 27 Marts, kl.22.00</p>
+                <p class="colorgreentext boldtext">32 deltagere, 7 interesseret</p>
+                <p class="location">Placering: Tusind års skoven, Odense C</p>
+              </div>
+            </a>
+          </figure>
+
+        </div>
+          </div>
+          <div class="row pb-5">
+  <a class="m-auto text-center event-item col-md-6" href="#">GÅ TIL AKTIVITETS KALENDEREN</a>
+      </div>
+</section>
+
+<!-- SEKTION 2 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+
+<hr>
+
+<!-- SEKTION 3 SLIDER OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+<section class="col-md-12 pt-5">
+  <div class="container">
+  <div class="row">
+        <div class="col-6 m-auto text-center">
+<img class="" src="assets/images/heart.svg" alt="" height="40">
+<h4 id="sliderheading">Udtalelser fra vores <span class="colorgreentext">talspersoner</span></h4>
+  </div>
+    </div>
+    </div>
+</section>
+
+<section id="slider" class="m-auto col-md-7">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="py-5 carousel-item active">
+      <img class="d-block img-fluid" src="assets/images/slider1.png" alt="First slide">
+    </div>
+    <div class="py-5 carousel-item">
+      <img class="d-block img-fluid" src="assets/images/slider2.png" alt="Second slide">
+    </div>
+    <div class="py-5 carousel-item">
+      <img class="d-block img-fluid" src="assets/images/slider1.png" alt="Third slide">
+    </div>
+  </div>
+</div>
+<div class="col-6 m-auto py-3 text-center">
+<a class="" id="sliderheadingexpand" href="#">GÅ TIL AKTIVITETS KALENDEREN</a>
+</div>
+</section>
+
+
+<!-- SEKTION 3 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+<hr>
+<!-- SEKTION 4  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+
+<section id="senestenyt" class="col-md-12">
+<div class="container pb-5">
+  <div class="row">
+    <div class="col-6 m-auto text-center py-5">
+      <h4 class="boldtext h2 mt-4 sliderheading"><span class="colorgreentext">Seneste nyt</span> fra LVS</h4>
+      <p class="pt-2">Følg med på hvad der sker via vores facebook nyheds feed som bliver opdateret dagligt. Smid eventuelt et like på vore facebook side.</p>
+    </div>
+  </div>
+      <div class="row">
+        <figure class="event-item col-md-4">
+          <a href="#">
+            <div class="event-image" style="background-image: url('assets/images/senesteny1.png');">
+              <div class="">
+              </div>
+            </div>
+            <div class="event-info px-3 py-4 col-sm-12">
+              <div class="btn-green-round">
+                <i class="read-more-btn fas fa-arrow-right"></i>
+              </div>
+              <p class="greyfontfacebookmockup ">5 timer siden | 21 synes godt om</p>
+              <p class="colorgreentext">Mød Michelle, som har fået et vendpunkt i livet efter at have haft problemer med misbrug og indlæggelse på psykiatrisk afdeling...</p>
+            </div>
+          </a>
+        </figure>
+
+        <figure class="event-item col-md-4 col-sm-12">
+          <a href="#">
+            <div class="event-image" style="background-image: url('assets/images/senesteny2.png');">
+              <div class="">
+              </div>
+            </div>
+            <div class="event-info px-3 py-4">
+              <div class="btn-green-round">
+                <i class="read-more-btn fas fa-arrow-right"></i>
+              </div>
+              <p class="greyfontfacebookmockup ">5 timer siden | 21 synes godt om</p>
+              <p class="colorgreentext">Kom og besøg Værestedet Regnbuen i Kolding og deltag i et af de spændende motionsevent såsom fodbold, rundbold, staffet og mange andre....</p>
+            </div>
+          </a>
+        </figure>
+
+        <figure class="event-item col-md-4 col-sm-12">
+            <div class="textboks p-4">
+          <p class="greyfontfacebookmockup">1 dag siden  |  34 synes godt om</p>
+          <p>Har du altid drømt at komme på den røde løber til en film premiere? - så skal du være med i vores konkurrence. <br>
+I samarbejde med DOXBiO udlover vi 2X2 billetter til premieren på filmen “I fars hænder”, som handler om instruktørens egen opvækst og forholdet til sin alkoholiseret far.
+Hvis du vinder billetterne får du også…</p>
+<div class="containerbuttonmid">
+<div class="btn-green-round-middle">
+  <i class="read-more-btn fas fa-arrow-right"></i>
+</div>
+</div>
+              </div>
+
+              </div>
+        </figure>
+
+      </div>
+        </div>
+        <div class="row pb-5">
+<a class="m-auto text-center event-item col-md-6" href="#">GÅ TIL NYHEDSFEED</a>
+    </div>
+</section>
+<!-- SEKTION 4 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+
+
+<!-- SEKTION 5  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+
+<section id="blivmedlemsektion" class= "col-md-12">
+<div class="container py-5">
+  <div class="row">
+    <div class="col-6 m-auto text-center">
+      <h1 class="boldtext h2 mt-4"><span class="colorgreentext">Bliv medlem af</span> LVS</h1>
+      <p class="mt-4">Som medlem kommer du på vores donations liste og får alle vores publikaitoner gratis.</p>
+    </div>
+  </div>
+  <div class="row text-center mt-5">
+    <div class="col-md-4">
+      <figure class="p-4">
+        <img class="mb-4" src="assets/images/star.svg" alt="" height="90">
+        <h2>ENGANGS BELØB</h2>
+        <p>Bliv et æres medlem og få muligheden for at få dit logo på hjemmesiden samt navn hvis det ønskes. Du vil blive inviteret til årlige arrangementer.</p>
+<p class="price">199<span class="dkk">DKK</span></p>
+        <p class="greytextsmall">engangsbeløb</p>
+        <div class="containerbuttonmid">
+        <div class="btn-green-round-middle">
+          <i class="read-more-btn fas fa-arrow-right"></i>
+        </div>
+        </div>
+      </figure>
+    </div>
+    <div class="col-md-4">
+      <figure class="p-4">
+        <img class="mb-4" src="assets/images/haenderdollar.svg" alt="" height="90">
+        <h2>DEN GAVMILDE</h2>
+        <p>Bliv et æres medlem og få muligheden for at få dit logo på hjemmesiden samt navn hvis det ønskes. Du vil blive inviteret til årlige arrangementer.</p>
+<p class="price">99<span class="dkk">DKK</span></p>
+        <p class="greytextsmall">engangsbeløb</p>
+        <div class="containerbuttonmid">
+        <div class="btn-green-round-middle">
+          <i class="read-more-btn fas fa-arrow-right"></i>
+        </div>
+        </div>
+      </figure>
+    </div>
+    <div class="col-md-4 mb-4">
+      <figure class="p-4">
+        <img class="mb-4" src="assets/images/diamond.svg" alt="" height="90">
+        <h2>ÆRESMEDLEMMET</h2>
+        <p>Bliv et æres medlem og få muligheden for at få dit logo på hjemmesiden samt navn hvis det ønskes. Du vil blive inviteret til årlige arrangementer.</p>
+        <p class="price">995<span class="dkk">DKK</span></p>
+        <p class="greytextsmall">engangsbeløb</p>
+        <div class="containerbuttonmid ">
+        <div class="btn-green-round-middle">
+          <i class="read-more-btn fas fa-arrow-right"></i>
+        </div>
+        </div>
+      </figure>
+    </div>
   </div>
 </div>
 </section>
 
+<!-- SEKTION 5 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
 <hr>
-
-
-<section>
-  <div class="col-6 m-auto text-center">
-    <img src="assets/images/heart.svg" alt="" height="40" width="40">
-    <h1 class="boldtext h2 mt-4"><span class="colorgreentext">Bliv medlem af LVS</span></h1>
-    <p>I Danmark er socialt udsatte stadivæk et velment fenomen som der skal gøres noget ved. Her kan du se en oversigt ov er hvad</p>
-  </div>
-</section>
-<!-- And so on -->
+<!-- SEKTION 6  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+  <section id="tilmeldingnyhedsbrev" class="col-md-12">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-10 m-auto formcontainer d-flex">
+        <div class="leftcontainernewsfeed col-5">
+          <div class="">
+        <h3 class="py-4"><span class="colorgreentext">Tilmeld dig</span> vores nyhedsbrev</h3>
+        <p>Følg med på hvad der sker via vores nyhedsbrev og bliv opdateret omkring events og nyheder.</p>
+        <form class="" action="index.html" method="post" name="Navn">
+    <input type="text" id="lname" name="lastname" placeholder="Navn">
+        <input type="text" id="lname" name="lastname" placeholder="E-mail">
+        </form>
+        <div class="d-flex justify-content-center">
+            <button type="button" name="button" class="my-3" id="newsfeedbutton">Find et Værested</button>
+                  </div>
+            </div>
+          </div>
+          <div class="rightcontainernewsfeed col-7" style="background-image: url('assets/images/signupformimage.png');">
+            <div class="">
+              </div>
+            </div>
+        </div>
+        </div>
+        </div>
+</section >
+<!-- SEKTION 6 END OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
 
 <?php include 'footer.php'; ?>
